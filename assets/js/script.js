18ee936740fa03da0812d1c389b48b2eb4cd3e61
MandieBot <amandabotinelly@gmail.com>
2022-11-12 09:27:03 -0700

    API call is functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,63 @@ var fieldInput = document.getElementById("city-input");
 
 var searchBtn = document.getElementById("search-form");
 
+// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+//api key = f18a21a46c14735a21d43be4f3afb792
+
+//test Austin
+//30.2711286 - lat
+//-97.7436995 - lon
+
+var weatherApi = "https://api.openweathermap.org/data/3.0/onecall?";
+
+var apiKey = "f18a21a46c14735a21d43be4f3afb792";
+
 //FUNCTIONS
+
+//responsible for getting the lat and lon for the city passed
+function grabCoordinates(city) {
+  //this will make the call to get the coordinates for that city
+
+  var apiCall = "http://api.openweathermap.org/geo/1.0/direct?q=austin&appid=f18a21a46c14735a21d43be4f3afb792";
+
+  fetch(apiCall)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      grabWeather(lat, lon);
+    });
+}
+
+//responsible for making api call with the user search term
+function grabWeather(lat, lon) {
+  console.log(lat);
+  console.log(lon);
+}
+var apiCall = weatherApi + "lat=30.2711286&" + "lon=-97.7436995&" + "units=imperial&" + "appid=f18a21a46c14735a21d43be4f3afb792";
+
+fetch(apiCall)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+//   //render the temp as an h1 to the user
+
+// //responsible for form submission by capturing user input
 function runSearch(e) {
   e.preventDefault();
   var field = fieldInput.value;
- 
+
+  //make an api call with that search term and confirm data is sent back
+  grabWeather(field);
+  grabCoordinates(field);
 }
 
-//EVENT LISTENERS
+// //EVENT LISTENERS
 searchBtn.addEventListener("submit", runSearch);
